ab4a89646f11d1ce02cb15b224df88b1d2168fa2
Eric W. Greene <eric@t4d.io>
2024-04-15 18:17:26 +0000

    add a small tweak to the exercise 1 solution

--- courseware/exercises/01_exercise/app.py
+++ courseware/exercises/01_exercise/app.py
@@ -1,13 +1,13 @@
 class BankAccount:
     def __init__(self, account_number: str, balance: float) -> None:
         # Initialize account_number and balance attributes
-        self._account_number = account_number
-        self._balance = balance
+        self.account_number = account_number
+        self.balance = balance
 
     # Define getter and setter methods for account_number and balance
     @property
     def account_number(self) -> str:
-        return self._account_number.upper()
+        return self.__account_number.upper()
 
     @account_number.setter
     def account_number(self, account_number: str) -> None:
@@ -15,33 +15,33 @@ class BankAccount:
             raise ValueError(
                 "Account number must be at least 10 characters long."
             )
-        self._account_number = account_number
+        self.__account_number = account_number
 
     @property
     def balance(self) -> float:
-        return self._balance
+        return self.__balance
 
     @balance.setter
     def balance(self, balance: float) -> None:
         if balance < 0:
             raise ValueError("Balance cannot be negative.")
-        self._balance = balance
+        self.__balance = balance
 
     def deposit(self, amount: float) -> None:
         if amount < 0.01:
             raise ValueError("Amount is invalid, must be a penny or more.")
-        self._balance += amount
+        self.__balance += amount
 
     def withdraw(self, amount: float) -> None:
         if amount < 0.01:
             raise ValueError("Amount is invalid, must be a penny or more.")
-        if amount > self._balance:
+        if amount > self.__balance:
             raise ValueError("Insufficient funds.")
-        self._balance -= amount
+        self.__balance -= amount
 
 
 def main() -> None:
-    bank_account = BankAccount("a2rT412", 100.0)
+    bank_account = BankAccount("a2rT412DF1234", -1)
     print(bank_account.account_number)
     print(bank_account.balance)
     bank_account.deposit(50.0)
